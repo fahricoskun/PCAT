41539a09207efac80c1bbcbe3c662a46aa1e1161
Fahri <fahricoskun354@outlook.com>
2024-07-15 01:43:58 +0300

    add models updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const ejs = require("ejs");
 const path = require("path");
+
+const Photo = require("./models/Photo");
 
 const app = express();
 
+//connect DB
+mongoose.connect("mongodb://localhost/pcat-test-db");
+
+
+//Template Engine
+app.set("view engine", "ejs");
+
 //Middlewares
 app.use(express.static("public"));
+//! urlencoded
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+//Routes
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({})
+  res.render("index", {
+    photos
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+//! add.ejs action="/photos" yönlendirmesini burada yakalıyorum
+app.post("/photos", async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect("/");
 });
 
 const port = 3000;
